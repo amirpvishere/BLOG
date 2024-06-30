0b536ff19aebd673ee546e1f99d911d76c438d01
Amirmahdy Pourvahaby <amirmahdypourvahaby@gmail.com>
2024-06-30 16:20:05 +0330

    update
    category for posts

--- post/models.py
+++ post/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
